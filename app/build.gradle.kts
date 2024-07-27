ca2676c6b4a4866530c84ce1b94294622eb53001
Samir Ramic <samir@weloveapps.de>
2024-07-27 22:24:05 +0200

    - [SR-01] Updated version catalog (added dependencies) - kotlin 2.0 - ksp - hilt - retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -30,12 +34,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
@@ -50,6 +56,11 @@ android {
 }
 
 dependencies {
+    /* Di: Hilt */
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    /* Di: Retrofit */
+    implementation(libs.retrofit)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
