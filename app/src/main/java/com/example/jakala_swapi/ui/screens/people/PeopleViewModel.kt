89b514d849a1634e0a31fe9f4433b0ec11698510
Samir Ramic <samir@weloveapps.de>
2024-07-31 12:58:19 +0200

    [SR-17 Refactored vm to use StateFlow because of [conventions] set by other vms in the project]

--- app/src/main/java/com/example/jakala_swapi/ui/screens/people/PeopleViewModel.kt
+++ app/src/main/java/com/example/jakala_swapi/ui/screens/people/PeopleViewModel.kt
@@ -1,16 +1,21 @@
 package com.example.jakala_swapi.ui.screens.people
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jakala_swapi.data.remotesource.Result
 import com.example.jakala_swapi.domain.GetPeopleUseCase
 import com.example.jakala_swapi.ui.PeopleUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,42 +23,34 @@ class PeopleViewModel @Inject constructor(
     private val peopleUseCase: GetPeopleUseCase,
 ) : ViewModel() {
 
-    private var _state = MutableStateFlow(PeopleUiState())
-    val state get() = _state.asStateFlow()
+    private var defaultUiState by mutableStateOf(PeopleUiState())
+    private var currentPage = MutableStateFlow(1)
 
-    fun loadPeople() {
-        viewModelScope.launch {
-            peopleUseCase.invoke(state.value.page)
-                .catch { _state.update { it.copy(isLoading = false, isError = true) } }
-                .collect() {
-                    when (it) {
-                        is Result.Error -> {
-                            _state.update { it.copy(isLoading = false, isError = true) }
-                        }
-
-                        Result.Loading -> {
-                            _state.update { it.copy(isLoading = true) }
-                        }
-
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val uiStateStateFlow: StateFlow<PeopleUiState> = currentPage
+        .flatMapLatest { page ->
+            peopleUseCase.invoke(page)
+                .map { result ->
+                    when (result) {
+                        is Result.Error -> defaultUiState.copy(isLoading = false, isError = true)
+                        Result.Loading -> defaultUiState.copy(isLoading = true)
                         is Result.Success -> {
-                            val items = it.data.results
-                            val page = it.data.next?.substringAfter("=")?.toInt() ?: 1
-
-                            _state.update {
-                                it.copy(
-                                    isLoading = false,
-                                    isError = false,
-                                    items = it.items.plus(items),
-                                    page = page
-                                )
-                            }
+                            val newItems = result.data.results
+                            val nextPage = result.data.next?.substringAfter("=")?.toInt() ?: 1
+                            defaultUiState = defaultUiState.copy(
+                                isLoading = false,
+                                isError = false,
+                                items = defaultUiState.items.plus(newItems),
+                                page = nextPage
+                            )
+                            defaultUiState
                         }
                     }
                 }
         }
-    }
+        .stateIn(viewModelScope, SharingStarted.Lazily, defaultUiState)
 
-    fun loadNextPage() {
-        loadPeople()
+    fun loadNextPage(page: Int) {
+        currentPage.value = page
     }
 }
