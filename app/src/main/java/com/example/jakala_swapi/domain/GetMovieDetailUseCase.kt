7a29864f4a7122a77e58ed964c22f81ccfe51f3e
Samir Ramic <samir@weloveapps.de>
2024-08-14 08:04:16 +0200

    - [SR-24] Usage of combine (combine does not have to be used like extension)

--- app/src/main/java/com/example/jakala_swapi/domain/GetMovieDetailUseCase.kt
+++ app/src/main/java/com/example/jakala_swapi/domain/GetMovieDetailUseCase.kt
@@ -7,16 +7,19 @@ import com.example.jakala_swapi.data.remotesource.Result
 import com.example.jakala_swapi.data.repository.SwapiRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class GetMovieDetailUseCase @Inject constructor(
     private val repository: SwapiRepository,
     private val movieDetailDao: MovieDetailDao
 ) {
-    operator fun invoke(id: String, title: String): Flow<Result<MovieDetail>> =
-        movieDetailDao.getMovieDetail(title).combine(repository.getMovieDetail(id)) { a, b -> a to b }
-            .map { (movieDetailEntity, movieDetail) ->
-                movieDetailEntity?.let { Result.Success(it.toMovieDetail()) } ?: movieDetail
-            }
+    operator fun invoke(id: String, title: String): Flow<Result<MovieDetail>> = combine(
+        movieDetailDao.getMovieDetail(title),
+        repository.getMovieDetail(id)
+    ) { a, b -> a to b }.map { (movieDetailEntity, movieDetail) ->
+        movieDetailEntity?.let { Result.Success(it.toMovieDetail()) } ?: movieDetail
+    }
 }
